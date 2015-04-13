a8a744d63a39c6fbc2eb180b9a83056674b50340
Karen055 <karen.harutyunyan@outlook.com>
2015-04-13 18:23:07 +0400

    6

---
+++ TaskScheduler/ITask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TaskScheduler
+{
+    interface ITask
+    {
+        string Id { get; set; }
+        string Name { get; set; }
+        bool Enabled { get; set; }
+        bool IsRunning { get; set; }
+        bool RunIfMissed { get; set; }
+        bool AllowConcurrent { get; set; }
+
+        ITask AddTrigger(ITrigger trigger);
+        ITask AddAction(Action action);
+        ITask AddJob(IJob job);
+        void Start();
+        void Stop();
+        
+    }
+
+    interface ITask<out T> : ITask
+    {
+        void AddAction(Action<T> action);
+    }
+}
